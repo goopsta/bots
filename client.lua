341ef5baf1b843d3961495aeb4865dad955df15a
goopsta <81267230+goopsta@users.noreply.github.com>
2022-08-07 02:05:07 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,2 +1,3 @@
 getgenv().main = "put your main's username here"
+getgenv().script = [[print("Hello World!")]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/goopsta/bots/main/script.lua"))()
